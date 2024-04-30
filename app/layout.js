0fc32ee7e7c508ab3d6ddc9b563190ac23a42eb0
Yongjun <wangyj641@gmail.com>
2024-04-30 21:28:44 +0800

    Init project

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./style.css";
+import Sidebar from "@/components/Sidebar";
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">
+          <div className="main">
+            <Sidebar />
+            <section className="col note-viewer">{children}</section>
+          </div>
+        </div>
+      </body>
+    </html >
+  );
+}
---
+++ components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default async function Sidebar() {
+  return (
+    <>
+      <section className="col sidebar">
+        <Link href={'/'} className="link--unstyled">
+          <section className="sidebar-header">
+            <img
+              className="logo"
+              src="/logo.svg"
+              width="22px"
+              height="20px"
+              alt=""
+              role="presentation"
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className="sidebar-menu" role="menubar">
+          {/* SideSearchField */}
+        </section>
+        <nav>
+          {/* SidebarNoteList */}
+        </nav>
+      </section>
+    </>
+  )
+}
