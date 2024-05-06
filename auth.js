6c7ad43a384606e6909abdb346870e4025d3b499
Yongjun <wangyj641@gmail.com>
2024-05-06 14:44:39 +0800

    Checking auth when trying to edit a note

--- auth.js
+++ auth.js
@@ -1,3 +1,14 @@
+// how to use this auth as middleware
+
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-export const { handlers, auth, signIn, signOut } = NextAuth({ providers: [GitHub] })
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname.startsWith("/note/edit")) return !!auth
+      return true
+    },
+  }
+})
